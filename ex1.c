ff5558d0921e237b6e3490b60dabb20de54a5484
Brent Perry <burentoperii@gmail.com>
2018-07-19 14:47:58 -0500

    Add additional print statements.

--- ex1.c
+++ ex1.c
@@ -7,6 +7,11 @@ int main(int argc, char *argv[])
   
   // this is also a comment
 	printf("You are %d miles away.\n", distance);
+	printf("There are %d bottles of beer on the wall.\n", distance);
+	printf("I got %d problems!\n", distance - 1);
+	printf("Running out of ideas.\n");
+	printf("This is how we do it.\n");
+	printf("The end.\n");
 	
 	return 0;
 }
